de613e2dfe6f523b98b6bea15dc20b28d3f449b1
Thiago Cabral Correia <thiagocabralcorreia@gmail.com>
2022-06-28 14:14:44 -0300

    feature: add edit contact action

--- src/pages/EditContact.js
+++ src/pages/EditContact.js
@@ -1,18 +1,65 @@
-import React, { useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const EditContact = () => {
   const { id } = useParams();
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
+  
+  const history = useNavigate();
+  const dispatch = useDispatch();
+  const contacts = useSelector((state) => state);  
+  const currentContact = contacts.find(
+    (contact) => contact.id === parseInt(id)
+  );
+
+  useEffect(() => {
+    setName(currentContact.name);
+    setEmail(currentContact.email);
+    setPhone(currentContact.phone);
+  }, [currentContact]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const checkEmailExists = contacts.find(
+      contact => contact.id !== currentContact.id && contact.email === email
+    );
+    const checkPhoneExists = contacts.find(
+      contact => contact.id !== currentContact.id && contact.phone === parseInt(phone)
+    );
+
+    if (!name || !email || !phone) {
+      return toast.warning('Please fill in all fields.');
+    } else if (checkEmailExists) {
+      return toast.error('This email already exists.');
+    } else if (checkPhoneExists) {
+      return toast.error('This phone number already exists.');
+    };
+
+    const data = {
+      id: currentContact.id,
+      name,
+      email,
+      phone
+    };
+    console.log({ data })
+
+    dispatch({ type: 'EDIT_CONTACT', payload: data });
+    toast.success('Contact updated successfully.');
+    history('/');
+
+  }
 
   return (
     <div className='container-fluid'>
-      <h1 className='text-center text-dark py-4 display-6'>Edit Contact { id }</h1>
+      <h1 className='text-center text-dark py-4 display-6'>Edit {currentContact.name}</h1>
       <div className='row'>
         <div className='col-md-6 p-5 mx-auto shadow'>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className='form-group'>
               <input
                 className='form-control my-3'
--- src/redux/reducers/contactReducer.js
+++ src/redux/reducers/contactReducer.js
@@ -1,14 +1,14 @@
 const initialState = [
   {
     id: 0,
-    name: 'Tommy Shelby',
-    email: 'tommy@peakyblinders.com',
+    name: 'John Smith',
+    email: 'johnsmith@email.com',
     phone: 987654321,
   },
   {
     id: 2,
-    name: 'Arthur Shelby',
-    email: 'arthur@peakyblinders.com',
+    name: 'Mary Ann',
+    email: 'maryann@email.com',
     phone: 987654322,
   },
 ];
@@ -17,6 +17,13 @@ const contactReducer = ( state = initialState, action) => {
   switch (action.type) {
     case 'ADD_CONTACT':
       state = [ ...state, action.payload ]
+      return state;
+    case 'EDIT_CONTACT':
+      const contactUpdated = state.map(contact => contact.id === action.payload.id
+          ? action.payload
+          : contact
+      )
+      state = contactUpdated;
       return state;
     default: return state;
   }
